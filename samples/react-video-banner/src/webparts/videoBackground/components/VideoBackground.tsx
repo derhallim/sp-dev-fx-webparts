2b86842167d891ffe0550e09e806ec6011b08f66
Mohamed Derhalli <derhallim@gmail.com>
2021-04-02 13:31:35 -0400

    Modify video refs to update video

--- samples/react-video-banner/src/webparts/videoBackground/components/VideoBackground.tsx
+++ samples/react-video-banner/src/webparts/videoBackground/components/VideoBackground.tsx
@@ -3,7 +3,7 @@ import styles from "./VideoBackground.module.scss";
 import { IVideoBackgroundProps } from "./IVideoBackgroundProps";
 
 const VideoBackground = (props: IVideoBackgroundProps) => {
-  const vRef = React.useRef(HTMLVideoElement.prototype) 
+  const vRef = React.useRef<HTMLVideoElement>(null) 
   const oldUrl = React.useRef(props.videoUrl)
   console.debug("execution here")
   
